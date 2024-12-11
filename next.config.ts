9641f3d60e558924328e268f1e2e7b2057781380
순민 <tkksm2@gmail.com>
2024-12-11 17:03:12 +0900

    init

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  generateBuildId: async () => {
+    return process.env.GIT_HASH ?? "";
+  },
 };
 
 export default nextConfig;
