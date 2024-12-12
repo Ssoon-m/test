3635b1d8e666b0045f13f4c5447357535df7e9a4
권순민 <soonmin@gwonsunmin-ui-MacBookPro.local>
2024-12-12 09:36:09 +0900

    add focus event

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  generateBuildId: async () => {
-    return process.env.GIT_HASH || "local-build-2";
-  },
+  // generateBuildId: async () => {
+  //   return process.env.GIT_HASH || "local-build-2";
+  // },
 };
 
 export default nextConfig;
--- src/pages/VersionChecker.tsx
+++ src/pages/VersionChecker.tsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from "react";
 
+const getBuildVersion = async () => {
+  return await fetch("/api/hello")
+    .then((res) => res.json())
+    .then((data) => data.buildId);
+};
+
 export default function VersionChecker() {
   const [currentBuildId, setCurrentBuildId] = useState<string | null>(null);
   const [latestBuildId, setLatestBuildId] = useState<string | null>(null);
   useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setCurrentBuildId(data.buildId));
-
-    const interval = setInterval(() => {
-      fetch("/api/hello")
-        .then((res) => res.json())
-        .then((data) => setLatestBuildId(data.buildId));
-    }, 1000);
-
-    return () => clearInterval(interval);
+    getBuildVersion().then((buildId) => setCurrentBuildId(buildId));
+    const handleFocusBuildVersion = () => {
+      getBuildVersion().then((buildId) => setLatestBuildId(buildId));
+    };
+    window.addEventListener("focus", handleFocusBuildVersion);
+    return () => window.removeEventListener("focus", handleFocusBuildVersion);
   }, []);
 
   console.log("currentBuildId: ", currentBuildId);
@@ -23,7 +24,7 @@ export default function VersionChecker() {
   useEffect(() => {
     if (currentBuildId && latestBuildId && currentBuildId !== latestBuildId) {
       alert("새 버전이 배포되었습니다. 페이지를 새로고침합니다.");
-      window.location.reload();
+      // window.location.reload();
     }
   }, [currentBuildId, latestBuildId]);
 
